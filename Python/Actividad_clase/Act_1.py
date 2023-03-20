f840b325ba3b8c7e0a3b01a6be47894c1187735a
Jonathan Delgado <jonathandelgado14092001@gmail.com>
2023-03-20 11:08:09 -0500

    subiendo actividades (1,2,3,4,5)

---
+++ Python/Actividad_clase/Act_1.py
@@ -0,0 +1,11 @@
+# su que de 10 
+
+myArray = (1,3,4,2,7,0)
+
+n = len(myArray)
+
+for i in range(n):
+    for j in range(n):
+        if (myArray[i] + myArray[j]) == 10:
+            print(f'{myArray[i]}  {myArray[j]}')
+
